6d8acf02427e2f60d3e5282a40c6adbf609474a7
Rodrigo Sampaio <88935188+RodrigoSam@users.noreply.github.com>
2021-08-24 23:53:16 -0300

    Add files via upload

---
+++ HoraDoSistema.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+class HoraDoSistema {
+
+	public static void main(String[] args) {
+		Date Relogio = new Date();
+		System.out.println("A Hora do Sistema é");
+		System.out.println(Relogio.toString());
+	}
+
+}
